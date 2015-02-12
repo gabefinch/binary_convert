8346c911cdc52f4dd3fe5340627b350cf7f0bf0a
Gabe Finch and Kathryn Carr <student@epicodus.com>
2015-02-12 15:19:58 -0800

    Add binToDec trinToDec and hexToDec functions

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+var binToDec = function(binary){
+  var result = 0;
+  var reverseBin = ((binary.split("")).reverse()).join("");
+
+  for(var i=0; i < reverseBin.length; i++){
+    if(reverseBin[i] == '1'){
+      result = result + Math.pow(2,i);
+    }
+  }
+
+  return result;
+
+};
+
+var trinToDec = function(trinary){
+  var result = 0;
+  var reverseTrin = ((trinary.split("")).reverse()).join("");
+
+  for(var i=0; i < reverseBin.length; i++){
+    if(reverseTrin[i] == '1'){
+      result = result + Math.pow(3,i);
+    } else if (reverseTrin[i] == '2'){
+      result = result + Math.pow(3,i) + Math.pow(3,i);
+    }
+  }
+
+  return result;
+
+};
+
+var hexToDec = function(hexadecimal){
+var result = 0;
+var reverseHex = ((hexadecimal.split("")).reverse()).join("");
+var hexDigits = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+for(var i=0; i < reverseHex.length; i++){
+  result = result + (Math.pow(16,i))*hexDigits.indexOf(reverseHex[i]);
+}
+
+return result;
+
+};
+
+
+var decToHex = function(decimal){
+
+  return 'a';
+
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,43 @@
 describe("binToDec", function(){
-  it ('returns 0 for input 0', function(){
+  it ("returns 0 for input '0'", function(){
     expect(binToDec("0")).to.equal(0);
+  });
+
+  it("returns 1 for input '1'", function(){
+    expect(binToDec("1")).to.equal(1);
+  });
+
+  it("returns 2 for input '10'", function(){
+    expect(binToDec("10")).to.equal(2);
+  });
+
+  it("returns 3 for input '11'", function(){
+    expect(binToDec("11")).to.equal(3);
+  });
+
+  it("returns 4 for input '100'", function(){
+    expect(binToDec("100")).to.equal(4);
+  });
+
+});
+
+describe("hexToDec", function(){
+  it ("returns 10 for input 'a'", function(){
+    expect(hexToDec("a")).to.equal(10);
+  });
+
+  it("returns 15 for input 'f'", function(){
+    expect(hexToDec('f')).to.equal(15);
+  });
+
+  it("returns 123442342 for input '75b94a6'", function(){
+    expect(hexToDec('75b94a6')).to.equal(123442342);
+  });
+
+});
+
+describe("decToHex", function(){
+  it("returns 'a' for input 10", function(){
+    expect(decToHex(10)).to.equal('a');
   });
 });
